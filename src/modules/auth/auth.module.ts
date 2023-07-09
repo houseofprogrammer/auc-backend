b9d4ac812e2648dc5eb46a1e2890331924b11354
mtaufan.dev <mtaufan.dev@gmail.com>
2023-07-09 20:23:57 +0700

    move auth and users to modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { LocalStrategy } from 'src/common/local.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from 'src/modules/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from 'src/common/jwt.strategy';
